7e578998409f2950598947aed5c1367c113bce7f
Leonardo Leyva <lleyva@intersog.com>
2022-05-21 15:50:28 -0600

    Initialize api

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+
+import mainRouter from './routes/main.routes'
+import './db/connection'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/', mainRouter)
+
+app.listen(8000, () => {
+  console.log('Running app')
+})
+
+export default app
