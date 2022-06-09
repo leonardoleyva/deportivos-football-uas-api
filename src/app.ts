e30163eee4605d038d2ec9c51cb8e396c838fcfe
leonardoleyva <leoshield9@gmail.com>
2022-06-09 04:48:58 -0600

    Increase payload limit

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ import mainRouter from './routes/main.routes'
 const app = express()
 
 app.use(cors())
-app.use(express.json())
+app.use(express.json({ limit: '5mb' }))
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/', mainRouter)
